b6e2bb8406fdae639f8e633e340c353eb6cbe5e0
Rémi Fabas <remi.fabasrhsolutions.ext@altran.com>
2019-12-20 16:30:50 +0100

    hello and connect to mongo

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
+// You will be using this Trainer type later in the program
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
+
+func main() {
+	// Set client options
+	client := getMongoClient("mongodb://localhost:27017")
+
+	collection := client.Database("test").Collection("trainers")
+
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	insertResult, err := collection.InsertOne(context.TODO(), ash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+}
---
+++ mongoConnect.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func getMongoClient(url string) *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(url)
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
